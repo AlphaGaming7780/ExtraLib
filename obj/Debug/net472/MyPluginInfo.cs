e4e55c7ac4372ed37df4113f28f8af4f2bd1729c
Alpha Gaming <69048433+AlphaGaming7780@users.noreply.github.com>
2024-03-10 16:00:49 +0100

    remove obj folder

--- obj/Debug/net472/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace ExtraDetailingTools
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "ExtraDetailingTools";
-        public const string PLUGIN_NAME = "ExtraDetailingTools";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
