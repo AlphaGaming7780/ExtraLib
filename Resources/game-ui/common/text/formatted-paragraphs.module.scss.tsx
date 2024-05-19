4b6f18cb89680994ccb3ee0b19354b0d4ea65866
Alpha Gaming <69048433+AlphaGaming7780@users.noreply.github.com>
2024-05-19 21:45:25 +0200

    game-ui

--- Resources/game-ui/common/text/formatted-paragraphs.module.scss.tsx
+++
@@ -1,7 +0,0 @@
-import { getModule } from "cs2/modding"
-
-const path$ = "game-ui/common/text/formatted-paragraphs.module.scss"
-
-export const FormattedParagraphsSCSS = {
-	paragraphs: getModule(path$, "classes").paragraphs,
-}
