10d32516f7d63e9577bfed9cfdb4612e4549acf3
Alpha Gaming <69048433+AlphaGaming7780@users.noreply.github.com>
2024-05-24 16:13:24 +0200

    notification Info

---
+++ mod/ClassExtension/NotificationUI.cs
@@ -0,0 +1,25 @@
+﻿using Game.UI.Menu;
+using static Game.UI.Menu.NotificationUISystem;
+
+namespace Extra.Lib.mod.ClassExtension;
+
+public static class NotificationUIExtension
+{
+    public static void AddOrUpdateNotification(this NotificationUISystem notificationUISystem, ref NotificationInfo notificationInfo)
+    {
+        notificationInfo = notificationUISystem.AddOrUpdateNotification(
+            notificationInfo.id, 
+            notificationInfo.title, 
+            notificationInfo.text, 
+            notificationInfo.thumbnail, 
+            notificationInfo.progressState, 
+            notificationInfo.progress, 
+            notificationInfo.onClicked
+        );
+    }
+
+    public static void Update(this NotificationInfo notificationInfo)
+    {
+        ExtraLib.m_NotificationUISystem.AddOrUpdateNotification(ref notificationInfo);
+    }
+}
