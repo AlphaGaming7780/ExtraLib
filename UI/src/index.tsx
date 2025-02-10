9f613657ae53f3f93fd3e269bd645c8d7a6e35df
AlphaGaming7780 <69048433+AlphaGaming7780@users.noreply.github.com>
2025-02-11 00:01:55 +0100

    Top left button test

--- UI/src/index.tsx
+++ UI/src/index.tsx
@@ -2,13 +2,14 @@ import { ModRegistrar } from "cs2/modding";
 import { HelloWorldComponent } from "mods/hello-world";
 import { ExtraDetailingDetails, ExtraAssetsMenu } from "./mods/ExtraAssetsMenu";
 import { AssetMultiCategory } from "./mods/AssetMultiCategory";
+import { ExtraToolsButton } from "./mods/ExtraTools";
 
 const register: ModRegistrar = (moduleRegistry) => {
 
     moduleRegistry.extend("game-ui/game/components/asset-menu/asset-category-tab-bar/asset-category-tab-bar.tsx", 'AssetCategoryTabBar', ExtraAssetsMenu)
     moduleRegistry.extend("game-ui/game/components/asset-menu/asset-category-tab-bar/asset-category-tab-bar.tsx", 'AssetCategoryTabBar', AssetMultiCategory)
     moduleRegistry.extend("game-ui/game/components/asset-menu/asset-menu.tsx", "AssetMenu", ExtraDetailingDetails)
-
+    moduleRegistry.append('GameTopLeft', ExtraToolsButton);
     moduleRegistry.append('Menu', HelloWorldComponent);
 }
 
---
+++ UI/src/mods/ExtraTools.tsx
@@ -0,0 +1,12 @@
+import { MenuButton } from "cs2/ui";
+import { IconButton } from "../../game-ui/common/input/button/icon-button";
+
+export const ExtraToolsButton = () => <div>
+
+    <div>
+        <IconButton
+            src={"coui://extralib/Icons/Icon.svg"}
+        />
+    </div>
+
+</div> 
